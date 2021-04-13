f2d74c2cf7f06cd9338a35fbd81280e895e12bb0
jjjjjacckk <zwillinge175@gmail.com>
2021-04-13 17:27:33 +0000

    add controller logic to p4runtime_controller.py

--- controller/p4runtime_controller.py
+++ controller/p4runtime_controller.py
@@ -18,7 +18,83 @@ import p4runtime_lib.helper
 
 global seq
 
-def WriteTableEntry():
+def WriteNATRule(P4InfoHelper, NATNumber):
+    # TODO: add table Entry
+    print("WriteNATRule")
+
+def set_send_frame(P4InfoHelper, nat, port, gateway):
+    table_entry = p4info_helper.buildTableEntry(
+        table_name="egress.send_frame", # NAME?!
+        match_fields={
+            "standard_metadata.egress_port": port # bit = 32?
+        },
+        action_name="egress.rewrite_mac",
+        action_params={
+            "smac": "08:00:00:00:00:%02d:00" % gateway,
+        })
+    nat.WriteTableEntry(table_entry)
+
+def set_forward(P4InfoHelper, nat, ipv4, number):
+    table_entry = p4info_helper.buildTableEntry(
+        table_name="ingress.forward", # NAME?!
+        match_fields={
+            "meta.routing_metadata.nhop_ipv4": ipv4
+        },
+        action_name="ingress.set_dmac",
+        action_params={
+            "dmac": "08:00:00:00:00:%02d:%d%d" % (number, number, number),
+        })
+    nat.WriteTableEntry(table_entry)
+
+def set_ipv4_lpm(P4InfoHelper, nat, ipv4, port):
+    #   - table_add ipv4_lpm set_nhop 10.0.2.2/32 => 10.0.2.2 2
+    #   - table_add ipv4_lpm set_nhop 140.116.0.1/32 => 140.116.0.1 3
+    #   - table_add ipv4_lpm set_nhop 140.116.0.2/32 => 140.116.0.2 4
+    table_entry = p4info_helper.buildTableEntry(
+        table_name="ingress.ipv4_lpm", # NAME?!
+        match_fields={
+            "hdr.ipv4.dstAddr": '%s/32' % ipv4
+        },
+        action_name="ingress.set_nhop",
+        action_params={
+            "nhop_ipv4": ipv4,
+            "port": port
+        })
+    nat.WriteTableEntry(table_entry)
+
+def set_fwd_nat_tcp(p4info_helper, nat, hostIP, h2nPort, NATIP, allocatePort):
+    # - table_add fwd_nat_tcp rewrite_srcAddrTCP HOST_IP HOST2NAT_PORT => NAT_IP ALLOCATE_PORT
+    table_entry = p4info_helper.buildTableEntry(
+        table_name="egress.fwd_nat_tcp", # NAME?!
+        match_fields={
+            "hdr.ipv4.srcAddr": hostIP,
+            "hdr.tcp.srcPort": h2nPort
+        },
+        action_name="egress.rewrite_srcAddrTCP",
+        action_params={
+            "ipv4Addr": NATIP,
+            "port": allocatePort
+        })
+    nat.WriteTableEntry(table_entry)
+
+def set_rev_nat_tcp(p4info_helper, nat, hostIP, h2nPort, NATIP, allocatePort):
+    # - table_add fwd_nat_tcp rewrite_srcAddrTCP HOST_IP HOST2NAT_PORT => NAT_IP ALLOCATE_PORT
+    table_entry = p4info_helper.buildTableEntry(
+        table_name="ingress.rev_nat_tcp", # NAME?!
+        match_fields={
+            "hdr.ipv4.dstAddr": NATIP,
+            "hdr.tcp.dstPort": allocatePort
+        },
+        action_name="ingress.rewrite_dstAddrTCP",
+        action_params={
+            "ipv4Addr": hostIP,
+            "tcpPort": h2nPort
+        })
+    nat.WriteTableEntry(table_entry)
+
+def WriteBasicRule(p4info_helper, nat1, nat2):
+    # TODO: connection between hosts and switches
+
     # [ ingress ]
     # send_frame : gateway MAC (frame -> don't know)
     # fwd_nat_tcp : rewrite packet source address
@@ -29,34 +105,54 @@ def WriteTableEntry():
     # match_nat_tcp : matching NAT IP table
     print '[ WriteTableEntry ]'
 
-    # [ ingress ]
-    #   - [ send_frame ]
-    #       - [ NAT_1 ]
-    #       - spec (connect to switch) -> gateway MAC
-    #           - table_add send_frame rewrite_mac 1 => 08:00:00:00:00:01:11
-    #           - table_add send_frame rewrite_mac 2 => 08:00:00:00:00:02:22
-    #           - table_add send_frame rewrite_mac 3 => 08:00:00:00:00:03:33
-    #           - table_add send_frame rewrite_mac 4 => 08:00:00:00:00:04:44
-    #   - [ forward ]
-    #       - table_add forward set_dmac 10.0.1.11 => 08:00:00:00:00:01:11
-    #       - table_add forward set_dmac 10.0.2.22 => 08:00:00:00:00:02:22
-    #       - table_add forward set_dmac 10.0.3.33 => 08:00:00:00:00:03:33
-    #       - table_add forward set_dmac 10.0.4.44 => 08:00:00:00:00:04:44
-    #       - table_add forward set_dmac 140.116.0.1 => 08:00:00:00:00:05:55
-    #       - table_add forward set_dmac 140.116.0.2 => 08:00:00:00:00:06:66
-    #   - [ ipv4_lpn ] 
-    #       - [ NAT_1 ]
-    #           - table_add ipv4_lpm set_nhop 10.0.1.11/32 => 10.0.1.11 1
-    #           - table_add ipv4_lpm set_nhop 10.0.2.22/32 => 10.0.2.22 2
-    #           - table_add ipv4_lpm set_nhop 140.116.0.1/32 => 140.116.0.1 3
-    #           - table_add ipv4_lpm set_nhop 140.116.0.2/32 => 140.116.0.2 4
-    #   - [ match_nat_ip ]
-    #       - to send NAT IP to controller ?!
-    #   - [ fwd_nat_tcp ]
-    #       - table_add fwd_nat_tcp rewrite_srcAddrTCP HOST_IP HOST2NAT_PORT => NAT_IP ALLOCATE_PORT
-    #   - [ rev_nat_tcp ]
-    #       - table_add rev_nat_tcp rewrite_dstAddrTCP NAT_IP ALLOCATE_PORT => HOST_IP HOST2NAT_PORT
+    # send_frame (NAT1)
+    set_send_frame(P4InfoHelper, nat1, 1, 1)
+    set_send_frame(P4InfoHelper, nat1, 2, 2)
+    set_send_frame(P4InfoHelper, nat1, 3, 5)
+    set_send_frame(P4InfoHelper, nat1, 4, 6)
 
+    # send_frame (NAT2)
+    set_send_frame(P4InfoHelper, nat2, 1, 3)
+    set_send_frame(P4InfoHelper, nat2, 2, 4)
+    set_send_frame(P4InfoHelper, nat2, 3, 5)
+    set_send_frame(P4InfoHelper, nat2, 4, 6)
+
+    # forward
+    for x in range(1, 5):
+        set_forward(P4InfoHelper, nat1, '10.0.%d.%d' % (x, x), '08:00:00:00:00:%02d:%d%d' % (x, x, x))
+        set_forward(P4InfoHelper, nat2, '10.0.%d.%d' % (x, x), '08:00:00:00:00:%02d:%d%d' % (x, x, x))
+    
+    for x in range(1, 3):
+        set_forward(P4InfoHelper, nat1, '140.116.0.%d' % x, '08:00:00:00:00:%02d:%d%d' % (x+4, x+4, x+4))
+        set_forward(P4InfoHelper, nat2, '140.116.0.%d' % x, '08:00:00:00:00:%02d:%d%d' % (x+4, x+4, x+4))
+
+    # ipv4_lpm
+    #   - table_add ipv4_lpm set_nhop 10.0.1.1/32 => 10.0.1.1 1
+    for x in range(1, 3):
+        set_forward(P4InfoHelper, nat1, '10.0.%d.%d' % (x, x), x)
+        set_forward(P4InfoHelper, nat2, '10.0.%d.%d' % (x+2, x+2), x)
+
+    for x in range(1, 3):
+        set_ipv4_lpm(P4InfoHelper, nat1, '140.116.0.%d' % x, x+2)
+        set_ipv4_lpm(P4InfoHelper, nat2, '140.116.0.%d' % x, x+2)
+    
+    # TODO: ALLCATION_PORT
+    # fwd_nat_tcp
+    # - table_add fwd_nat_tcp rewrite_srcAddrTCP HOST_IP HOST2NAT_PORT => NAT_IP ALLOCATE_PORT
+    set_fwd_nat_tcp(P4InfoHelper, nat1, '10.0.1.1', 1, '140.116.0.3', ALLOCATE_PORT_NAT1_1)
+    set_fwd_nat_tcp(P4InfoHelper, nat1, '10.0.2.2', 2, '140.116.0.3', ALLOCATE_PORT_NAT1_2)
+    set_fwd_nat_tcp(P4InfoHelper, nat2, '192.168.3.3', 1, '140.116.0.4', ALLOCATE_PORT_NAT2_1)
+    set_fwd_nat_tcp(P4InfoHelper, nat2, '192.168.4.4', 2, '140.116.0.4', ALLOCATE_PORT_NAT2_2)
+
+    # TODO: ALLCATION_PORT
+    # rev_nat_tcp
+    # - table_add rev_nat_tcp rewrite_dstAddrTCP NAT_IP ALLOCATE_PORT => HOST_IP HOST2NAT_PORT
+    set_fwd_nat_tcp(P4InfoHelper, nat1, '10.0.1.1', 1, '140.116.0.3', ALLOCATE_PORT_NAT1_1)
+    set_fwd_nat_tcp(P4InfoHelper, nat1, '10.0.2.2', 2, '140.116.0.3', ALLOCATE_PORT_NAT1_2)
+    set_fwd_nat_tcp(P4InfoHelper, nat2, '192.168.3.3', 1, '140.116.0.4', ALLOCATE_PORT_NAT2_1)
+    set_fwd_nat_tcp(P4InfoHelper, nat2, '192.168.4.4', 2, '140.116.0.4', ALLOCATE_PORT_NAT2_2)
+
+    # TODO: pass ALLOCATION_PORT to switch
 
 
 def main(p4info_file_path, bmv2_file_path):
